9a43c370ed9b39dc68d9e0ff7b613cea3e4833cf
Umut Berkay Soydan <ubsoydan@protonmail.com>
2023-03-19 14:50:58 +0300

    add an area to drag widgets on

---
+++ components/WidgetArea.jsx
@@ -0,0 +1,21 @@
+"use client";
+import { useSpring, animated } from "@react-spring/web";
+import { useDrag } from "@use-gesture/react";
+import DragMe from "@/components/DragMe";
+
+export default function DragButton() {
+    const buttonPos = useSpring({ x: 0, y: 0 });
+    const bindButtonPos = useDrag((params) => {
+        buttonPos.x.set(params.offset[0]);
+        buttonPos.y.set(params.offset[1]);
+    });
+
+    return (
+        <animated.div
+            {...bindButtonPos()}
+            style={{ x: buttonPos.x, y: buttonPos.y }}
+        >
+            <DragMe />
+        </animated.div>
+    );
+}
